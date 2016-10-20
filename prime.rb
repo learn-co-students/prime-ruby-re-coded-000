753c296945429be310051e6705a2513fe9017a82
Ayedh Gaser <alshmary_ad@yahoo.com>
2016-10-20 11:58:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+def prime?(number)
+  k=2
+  flag=0
+  if  number > 1
+while k <= number/2
+    if number%k == 0
+     flag=1
+     break;
+     end
+  k += 1
+  end
+
+  if flag == 0
+    return true
+  else
+    return false
+  end
+else
+  return false
+end
+end
+  puts prime?(0)
