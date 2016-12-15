b640bc665afa35400f02c44050ebd960ee8c6db2
Abdalkhalek ahmed <eng.abdalkalk@gmail.com>
2016-12-15 19:00:22 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,31 @@
 # Add  code here!
+#def prime_checker(number)
+#  is_prime = true
+#  for i in 2..number-1
+#    if number % i == 0
+#      is_prime = false
+#    end
+#  end
+#  if is_prime
+#  false
+    #puts "#{number} is prime!"
+#  else
+#    true
+    #puts "#{number} is not prime."
+#  end
+#end
+#prime_checker(10)
+def prime?(n)
+  if n > 1
+    e = 2
+    while e < n
+      if n % e ==0
+        return false
+      end
+      e += 1
+    end
+    return true
+  else
+    return false
+  end
+end
