0a3e788f6daf21d15a79d083f4f3f053245c8068
dina aziz <dina.aziz100@yahoo.com>
2016-12-26 15:50:31 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(number)
+  if number <=1
+    return false
+end
+range= 2..Math.sqrt(number)
+my_range = range.to_a
+my_range.all? do |num|
+  number % num !=0
+end
+end
