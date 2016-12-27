d632f5761a2f1a8714bcd447b828458cacdd11b9
omar suliman <omarsulyman@gmail.com>
2016-12-27 20:24:51 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  if n > 1
+    e = 2
+    while e < n
+      if n % e ==0
+        return false
+      end
+      e += 1
+    end
+    return true
+  else
+    return false
+  end
+end
