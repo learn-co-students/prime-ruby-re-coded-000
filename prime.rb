58a2d8f7d6f4655908b747e93e688270689d8bcb
Ahmed ALFLAHY <lfight522@gmail.com>
2017-01-07 19:31:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  h=2
+  if number > 1
+    while h <= number/2
+      if number%h==0
+        flag=true
+        break
+       end
+    h+=1
+    end
+    if flag
+     return false
+  else
+      return true
+  end
+else
+  return false
+end
+end
