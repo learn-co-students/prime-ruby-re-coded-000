98304888a0b90c05e24c9cc30d681f46a712381f
Sewar Abdullah <abdullahsewar@gmail.com>
2017-01-08 07:48:36 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,17 +1,12 @@
 # Add  code here!
-#def prime?number
-#  (2..number-1).each do |num|
-#    if number%num == 0
-#      return false
-#    else
-#      return true
-#    end
-#  end
-#end
-def prime?(number)
-  if number <=1
+def prime?number
+  if number<=1
     return false
   end
-  numbers=(2..Math.sqrt(number)).to_a
-  numbers.all?{|n| number%n!=0}
+  (2..Math.sqrt(number)).each do |num|
+    if number%num == 0
+      return false
+    end
+  end
+  return true
 end
