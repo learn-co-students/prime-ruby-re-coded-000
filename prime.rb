fd0307c3a5c39ecaa107daf2745f35bf57084def
novenOo <noven5823@gmail.com>
2017-01-14 03:53:27 -0800

    Update prime.rb

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+# Add  code here!# Add  code here!# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+    range= 2 .. Math.sprt(number)
+    my_range = range.to_i
+    my_range.all? do |num|
+      number% num !=0
+    end
+  end
+end
